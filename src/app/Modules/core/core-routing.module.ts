e92563c7347e3db4d157ae75a6f83749d5c3b0b0
Sonisas <sonisas03@gmail.com>
2024-11-10 19:05:52 -0300

    Upload

--- src/app/Modules/core/core-routing.module.ts
+++ src/app/Modules/core/core-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DreamViewComponent } from './Pages/dream-view/dream-view.component';
 import { DreamEditComponent } from './Pages/dream-edit/dream-edit.component';
+import { DreamListComponent } from './Pages/dream-list/dream-list.component';
 
 const routes: Routes = [
-  { path:'view', component: DreamViewComponent }
+  { path:'view', component: DreamViewComponent },
+  { path:'list', component: DreamListComponent }
 ];
 
 @NgModule({
